999aa1ae5f3465e10b385628c543c1993ade6d35
Ivan Nenashev <inenashev@icloud.com>
2022-09-28 17:22:52 +0300

    4 пробела вместо 8

--- rms/__init__.py
+++ rms/__init__.py
@@ -5,13 +5,14 @@ from rms.users.models import User
 
 
 def create_app():
-        app = Flask(__name__)
-        app.config.from_pyfile('config.py')
-        db.init_app(app)
-        migrate = Migrate(app,db)
-        #TODO delete route
-        @app.route('/')
-        def index():
-                return 'Hello world'
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+    migrate = Migrate(app, db)
 
-        return app
+    # TODO delete route
+    @app.route('/')
+    def index():
+        return 'Hello world'
+
+    return app
