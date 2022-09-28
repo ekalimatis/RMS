0726242a75f27959aceb52d5be2dd277647bdef0
Ivan Nenashev <inenashev@icloud.com>
2022-09-28 17:35:35 +0300

    вынес токен в .env обновил requirements.txt

--- rms/config.py
+++ rms/config.py
@@ -1,10 +1,13 @@
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'rms.db')
 
-
-
-SECRET_KEY = 'AqSG7DknOsciMVHf'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SECRET_KEY = os.getenv("SECRET_KEY")
