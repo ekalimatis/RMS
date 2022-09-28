bc2fd00817809fb36e90587f6b5b3b29d33246c3
Ivan Nenashev <inenashev@icloud.com>
2022-09-28 13:32:55 +0300

    add migrations

--- rms/__init__.py
+++ rms/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
+from flask_migrate import Migrate
+from rms.db import db
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-        return 'Hello world'
+
 
 
 
 def create_app():
         app = Flask(__name__)
         app.config.from_pyfile('config.py')
+        db.init_app(app)
+        migrate = Migrate(app,db)
+        #TODO delete route
+        @app.route('/')
+        def index():
+                return 'Hello world'
+
         return app
