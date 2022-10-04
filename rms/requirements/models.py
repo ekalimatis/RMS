bfa45f5bed2ad0e3559263d8884927ea2e115efc
EKA <eka@limantis.com>
2022-10-04 09:02:27 +0300

    requirement_tree_node -> requirement_tree_nodes

--- rms/requirements/models.py
+++ rms/requirements/models.py
@@ -15,7 +15,7 @@ class Project(db.Model):
     name = db.Column(db.String(240), nullable=False)
     description = db.Column(db.Text())
     created_date = db.Column(db.DateTime(), nullable=False, server_default=text('(now() at time zone \'utc0\')'))
-    requirement_tree_node = relationship("RequirementTree", backref="project")
+    requirement_tree_nodes = relationship("RequirementTree", backref="project")
 
     def __repr__(self):
         return self.name
