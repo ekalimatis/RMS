ceacca4df66b0d7ab278cd6932cf1d7cf566af67
Ivan Nenashev <inenashev@icloud.com>
2022-10-05 20:53:38 +0300

    что то немного не работает)

--- rms/__init__.py
+++ rms/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_migrate import Migrate
-
+from flask_login import LoginManager
 
 from rms.db import db
 from rms.users.models import User
 from rms.requirements.models import Requirement
+from rms.users.views import blueprint as user_blueprint
 
 
 def create_app():
@@ -12,4 +13,13 @@ def create_app():
     app.config.from_pyfile('config.py')
     db.init_app(app)
     migrate = Migrate(app, db,  compare_server_default=True)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'users.login'
+    app.register_blueprint(user_blueprint)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
     return app
---
+++ rms/users/views.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template, flash, redirect, url_for
+from flask_login import login_user, logout_user, current_user
+
+from rms.db import db
+from rms.users.models import User
+from rms.users.forms import LoginForm
+
+blueprint = Blueprint('user', __name__, url_prefix='/users')
+
+
+@blueprint.route('/login')
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('user/index.html'))
+    title = 'Авторизация'
+    login_form = LoginForm()
+    return render_template('user/login.html',
+                           page_title=title,
+                           form=login_form)
+
