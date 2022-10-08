d93c5fa341a1a31a0aa90451fbe1c8751f6a92cc
EKA <eka@limantis.com>
2022-10-08 18:29:32 +0300

    Reaname endpoint for requirement list

--- rms/requirements/requirements.py
+++ rms/requirements/requirements.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+from operator import itemgetter
+
+from sqlalchemy import func
 
 from rms.requirements.models import db, RequirementTree, Requirement
 from rms.requirements.forms import RequirementForm
@@ -51,3 +54,32 @@ def make_requirements_list(project_id:int) -> list:
         requirement_list.append({'id': node_id, 'name': requirement_chain})
 
     return requirement_list
+
+def get_plain_requirement_text(project_id:int) -> str:
+
+    max_requirement_level = \
+    db.session.query(func.max(RequirementTree.level)).filter(RequirementTree.project_id == 1).one()[0]
+
+    requirement_nodes_list = db.session.query(RequirementTree).order_by(RequirementTree.level).order_by(
+        RequirementTree.created_date).all()
+
+    requirement_list = []
+    requirement_dict = {}
+
+    for node in requirement_nodes_list:
+        node_index = 10 ** (max_requirement_level - node.level)
+        if node.parent_id in requirement_dict:
+            requirement_dict[node.parent_id][2] += 1
+            node_index = requirement_dict[node.parent_id][2] * node_index + requirement_dict[node.parent_id][0]
+        requirement_dict[node.id] = [node_index, node, 0]
+        requirement_list.append((node_index, node,))
+
+    requirement_list.sort(key=itemgetter(0))
+
+    text = ''
+    for node in requirement_list:
+        requirement = db.session.query(Requirement).filter(Requirement.requirement_id == node[1].id).order_by(Requirement.created_date.desc()).one()
+        indent = '&nbsp;' * len(str(node[0]).replace('0',''))
+        text += f"{indent}{'.'.join(str(node[0]).replace('0',''))} {requirement.name}<br>{indent}{indent}{requirement.description}<br>"
+
+    return  text
