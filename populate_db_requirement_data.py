edb7d6b55efb38706509f19ae4f2326c128eae82
Ivan Nenashev <inenashev@icloud.com>
2022-10-23 19:41:51 +0400

    быстрый фикс

--- populate_db_requirement_data.py
+++ populate_db_requirement_data.py
@@ -5,23 +5,29 @@ from rms import create_app
 app = create_app()
 
 with app.app_context():
-    new_req_types = [RequirementTypes(type="черновик"),
-                     RequirementTypes(type="функциональное требование"),
-                     RequirementTypes(type="не функциональное требование")]
+    new_req_types = [
+        RequirementTypes(type="черновик"),
+        RequirementTypes(type="функциональное требование"),
+        RequirementTypes(type="не функциональное требование")
+    ]
 
     for type in new_req_types:
         db.session.add(type)
 
-    new_req_statuses = [RequirementStatuses(status="утверждено"),
-                        RequirementStatuses(status="отклонено"),
-                        RequirementStatuses(status="на рассмотрении")]
+    new_req_statuses = [
+        RequirementStatuses(status="утверждено"),
+        RequirementStatuses(status="отклонено"),
+        RequirementStatuses(status="на рассмотрении")
+    ]
 
     for status in new_req_statuses:
         db.session.add(status)
 
-    new_req_priorities = [RequirementPriority(priority="высокий"),
-                          RequirementPriority(priority="низкий"),
-                          RequirementPriority(priority="средний")]
+    new_req_priorities = [
+        RequirementPriority(priority="высокий"),
+        RequirementPriority(priority="низкий"),
+        RequirementPriority(priority="средний")
+    ]
 
     for priority in new_req_priorities:
         db.session.add(priority)
