ecdc8e7276c8e063b7e8113a11633fc38cbcdc00
Ivan Nenashev <inenashev@icloud.com>
2022-10-23 19:38:31 +0400

    быстрый фикс

--- rms/static/js/req_tree.js
+++ rms/static/js/req_tree.js
@@ -1,19 +1,16 @@
+console.log("js start")
 let jstree_div = document.getElementById('jstree_div');
-let url = '/requirements/tree_data/1';
-
-
-
-
-
-
-jstree_div.addEventListener('load', function (){
+let url = '/requirements/tree_data/' + jstree_div.getAttribute('project_id')
+function draw_tree(){
     fetch(url).then(function (response){
-        let r = response.json()
-        console.log(r)
+        response.json().then(
+            function(r){jQuery('#jstree_div').jstree(
+                {'core':{'data':r.data}}
+            )}
+        )
     })
-
-})
-
+}
+draw_tree()
 
 
 /*
